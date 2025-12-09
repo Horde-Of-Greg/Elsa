b8587571101eb600c4707c2e4d6be3ad937882dd
Alessian Dormal <alessian@alessian.be>
2025-12-09 03:24:12 +0100

    this was ugly

--- src/db/seeding/Seeder.ts
+++ src/db/seeding/Seeder.ts
@@ -1,9 +1,10 @@
 import { SeederConfig } from '../../config/schema';
+import { app } from '../../core/App';
 
 export class Seeder {
     constructor(private readonly config: SeederConfig) {}
 
     async seed() {
-        console.log('seeder empty for now');
+        app.core.logger.simpleLog('warn', 'Seeder Empty.');
     }
 }

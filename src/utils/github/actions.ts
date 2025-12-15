66081a9e8d73d59b4f27f865858b8996dbcc5dbd
Alessian Dormal <alessian@alessian.be>
2025-12-15 04:10:43 +0100

    I'm so stupid.......

--- src/utils/github/actions.ts
+++ src/utils/github/actions.ts
@@ -2,6 +2,6 @@ export function requireActionsEnv() {
     if (process.env.ENVIRONMENT !== "actions") {
         // eslint-disable-next-line no-console
         console.error('Tried to run an actions script inside a dev other than "actions".');
+        process.exit(1);
     }
-    process.exit(1);
 }

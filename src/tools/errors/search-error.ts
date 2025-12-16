5b10d14e1dc6bb98d16a68fd8ccc5881abd44dd0
Alessian Dormal <alessian@alessian.be>
2025-12-16 13:00:37 +0100

    add base impl for an error lookup tool

---
+++ src/tools/errors/search-error.ts
@@ -0,0 +1,52 @@
+import fs from "fs";
+import path from "path";
+
+import { env } from "../../config/env";
+import { core } from "../../core/Core";
+
+if (env.ENVIRONMENT !== "development") process.exit(1);
+const ARG: string | undefined = process.argv[2];
+if (!ARG) {
+    core.logger.error(
+        "Tried to run search-error without args. Usage: npm run tools:search-error -- <ERROR_CODE>",
+    );
+}
+
+const ROOT_DIR = process.cwd();
+const SRC = path.join(ROOT_DIR, "src");
+//prettier-ignore
+const CLASS_MATCHER = new RegExp(`export class ([A-Z][A-Za-z]+) extends [A-Z][A-Za-z]+ {(?:\n\\s*(?:readonly )?httpStatus\\s?=\\s?\\d{3};?)?\n\\s*(?:readonly )?code\\s?=\\s?"${ARG}";?`)
+const USAGE_MATCHER = (className: string) => new RegExp(`throw new ${className}`);
+
+void main();
+
+async function main() {
+    const MATCHING_CLASSES = await searchDir(SRC, CLASS_MATCHER);
+    for (const matching_class of MATCHING_CLASSES) {
+        const matches = await searchDir(SRC, USAGE_MATCHER(matching_class.match[1]));
+        for (const match of matches) {
+            core.logger.debug(`Found ${ARG} in ${match.fileName.replace(/(?:\/?\w+\/)*src\/(.+)/, "$1")}`);
+        }
+    }
+}
+
+async function searchDir(
+    dir: string,
+    toFind: RegExp,
+): Promise<{ match: RegExpMatchArray; fileName: string }[]> {
+    const matches: { match: RegExpMatchArray; fileName: string }[] = [];
+    const children = await fs.promises.readdir(dir);
+    for (const child of children) {
+        const pathed_child = path.join(dir, child);
+        const stats = await fs.promises.stat(pathed_child);
+        if (stats.isDirectory()) {
+            const result = await searchDir(pathed_child, toFind);
+            if (result !== null) matches.push(...result);
+            continue;
+        }
+        const file = await fs.promises.readFile(pathed_child, "utf-8");
+        const match = file.match(toFind);
+        if (match !== null) matches.push({ match: match, fileName: pathed_child });
+    }
+    return matches;
+}

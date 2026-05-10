1e4f0f18e193fcbd6cbceeb07855d3a41ed325bf
Alessian Dormal <alessian@alessian.be>
2026-05-10 16:22:03 +0200

    refactor the fetchLatestRemoteTag func to have more accurate guard checks, and have less assumptions about the type of the response

--- src/utils/github/tags.ts
+++ src/utils/github/tags.ts
@@ -2,24 +2,53 @@ import { MalformedResponseError } from "../../errors/internal/http";
 
 export async function fetchLatestRemoteTag(): Promise<string> {
     const response = await fetch("https://api.github.com/repos/horde-of-greg/elsa/tags");
-    const tags = (await response.json()) as object[];
-    const firstTag = tags[0];
+    const tags = await validTagsResponseGuardCheck(response);
+    const firstTag = validTagGuardCheck(tags[0]);
 
-    if (!("name" in firstTag)) {
+    return firstTag.name;
+}
+
+async function validTagsResponseGuardCheck(response: Response): Promise<unknown[]> {
+    const tags: unknown = await response.json();
+
+    if (!Array.isArray(tags)) {
         throw new MalformedResponseError({
-            objectTested: "latest remote tag",
+            objectTested: "github response",
             source: "GitHub",
+            failedCondition: "response is not an array",
+        });
+    }
+
+    return tags;
+}
+
+function validTagGuardCheck(tag: unknown): Record<"name", string> {
+    const objectTested = "latest remote tag";
+    const source = "GitHub";
+
+    if (typeof tag !== "object" || tag === null) {
+        throw new MalformedResponseError({
+            objectTested,
+            source,
+            failedCondition: "first tag is not an object",
+        });
+    }
+
+    if (!("name" in tag)) {
+        throw new MalformedResponseError({
+            objectTested,
+            source,
             failedCondition: "first tag is missing the 'name' property",
         });
     }
 
-    if (typeof firstTag.name !== "string") {
+    if (typeof tag.name !== "string") {
         throw new MalformedResponseError({
-            objectTested: "latest remote tag",
-            source: "GitHub",
+            objectTested,
+            source,
             failedCondition: "property 'name' in first tag is not a string",
         });
     }
 
-    return firstTag.name;
+    return tag as Record<"name", string>;
 }

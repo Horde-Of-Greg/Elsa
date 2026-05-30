c09c671998a8cc6816a9580e3e794414b0d92841
Alessian Dormal <alessian@alessian.be>
2026-05-30 13:38:23 +0200

    [CI] (tools) Fix Dependabot And Fix Commit Analysis (#73)

--- tools/utils/types/constructors/versioning/title.ts
+++ tools/utils/types/constructors/versioning/title.ts
@@ -1,9 +1,16 @@
 import type { AcceptedTitleScope, AcceptedTitleType } from "../../../../types/versioning/title.js";
+import { acceptedTitleScopes, acceptedTitleTypes } from "../../../../versioning/constants.js";
 
 export function makeAcceptedTitleType(titleType: string): AcceptedTitleType {
+    if (!(acceptedTitleTypes as readonly string[]).includes(titleType)) {
+        throw new Error(`Received Invalid Title Type: ${titleType}`);
+    }
     return titleType as AcceptedTitleType;
 }
 
 export function makeAcceptedTitleScope(titleScope: string): AcceptedTitleScope {
+    if (!(acceptedTitleScopes as readonly string[]).includes(titleScope)) {
+        throw new Error(`Received Invalid Title Type: ${titleScope}`);
+    }
     return titleScope as AcceptedTitleScope;
 }
